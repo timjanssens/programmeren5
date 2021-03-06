cd24940a301da745352ce39c20be6e5be931e201
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-03-06 12:18:04 +0100

    Add locations school from online JSON

--- FunctioneelProgrammerenEnJson/script/script.js
+++ FunctioneelProgrammerenEnJson/script/script.js
@@ -1,3 +1,7 @@
+
+
+
+
 const mijnKaart = L.map('kaart-id')
 // Laad de basiskaart
 const mijnBasisKaart = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -17,5 +21,23 @@ mijnBasisKaart.addTo(mijnKaart)
 // hoe groter het getal, hoe gedetailleerder
 mijnKaart.setView([51.2191, 4.40111], 12);
 
-const eersteMarkering = L.marker([ 51.28256053326298, 4.402915267575786], {icon: schoolIcon}).addTo(mijnKaart);
-const tweedeMarkering = L.marker([51.19420966668424, 4.4335459741221666], {icon: schoolIcon}).addTo(mijnKaart);
+// const eersteMarkering = L.marker([ 51.28256053326298, 4.402915267575786], {icon: schoolIcon}).bindPopup(
+// const tweedeMarkering = L.marker([51.19420966668424, 4.4335459741221666], {icon: schoolIcon}).addTo(mijnKaart);
+
+
+const request = new XMLHttpRequest()
+request.open('GET', 'https://opendata.arcgis.com/datasets/0047052fbbe94040bfe0b9fcad635212_636.geojson', true)
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  const data = JSON.parse(this.response)
+
+  console.log(data);
+
+  L.geoJSON(data).addTo(mijnKaart);
+
+
+
+}
+
+request.send();
