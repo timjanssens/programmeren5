54ea5195299709f5a9008bce64ad5c9457d80107
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-04-10 18:26:30 +0200

    Making seperate files

--- NodejsNpm/node-serieus/tj-modules/app/index.js
+++ NodejsNpm/node-serieus/tj-modules/app/index.js
@@ -1,14 +1,7 @@
-var Marvel = require('marvel')
- 
-var marvel = new Marvel({ publicKey: "cfc3e03eb8e709428113bbc27f142e67", privateKey: "3cbfc6bdaa7b0deeebf849a8f709adb82e8e8477"})
- 
-//show the description of the character
-marvel.characters
-  .name("Hulk")
-  .get(function(err, resp) {
-    if (err) { console.log("Error: ", err) }
-    else { console.log(resp[0].description) }
-  })
+const character = require('./character')
 
-// more info about the npm can be found at:
-// https://www.npmjs.com/package/marvel
+const player = "Hulk"
+
+const description = character.showDescription(player)
+
+console.log('The description of ' + player + ' is: ' + description);
