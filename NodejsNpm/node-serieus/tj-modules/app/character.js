33b81d9da558c5f1c57b9a0164dfdb4e5ea6ade0
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-04-11 08:54:46 +0200

    Trying to fix fuction in seperate module

--- NodejsNpm/node-serieus/tj-modules/app/character.js
+++ NodejsNpm/node-serieus/tj-modules/app/character.js
@@ -3,7 +3,7 @@ var Marvel = require('marvel')
 var marvel = new Marvel({ publicKey: "cfc3e03eb8e709428113bbc27f142e67", privateKey: "3cbfc6bdaa7b0deeebf849a8f709adb82e8e8477"})
 
 function showDescription(character) {
-    return marvel.characters
+    marvel.characters
       .name(character)
       .get(function(err, resp) {
         if (err) { console.log("Error: ", err) }
@@ -11,3 +11,14 @@ function showDescription(character) {
       })
 }
 module.exports.showDescription = showDescription;
+
+
+// function showDescription(character) {
+//   marvel.characters
+//     .name(character)
+//     .get(function (err, resp) {
+//       if (err) { return ("Error: ", err) }
+//       else { return (resp[0].description) }
+//     })
+// }
+// module.exports.showDescription = showDescription;
