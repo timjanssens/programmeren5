77aa59cecd3c6637dc3c07a85d0010fb1a62c814
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-04-17 07:03:16 +0200

    Add solution undefined solved

--- NodejsNpm/node-serieus/tj-modules/app/character.js
+++ NodejsNpm/node-serieus/tj-modules/app/character.js
@@ -2,19 +2,39 @@ var Marvel = require('marvel')
  
 var marvel = new Marvel({ publicKey: "cfc3e03eb8e709428113bbc27f142e67", privateKey: "3cbfc6bdaa7b0deeebf849a8f709adb82e8e8477"})
 
-function showDescription(character) {
-    marvel.characters
-      .name(character)
-      .get(function(err, resp) {
-        if (err) { console.log("Error: ", err) }
-        else { console.log(resp[0].description) }
-      })
+
+function getDescription(character) {
+  return new Promise(function (myResolve, myReject) {
+      // "Producing Code" (May take some time)
+      marvel.characters
+          .name(character)
+          .get(function (err, resp) {
+              if (err) {
+                  myReject(`code: ${err.code} / message: ${err.message}`);
+              }
+              else {
+                  myResolve(`Beschrijving ${character} : ${resp[0].description}`);
+              };
+          });
+  });
 }
-module.exports.showDescription = showDescription;
+
+
+module.exports.getDescription = getDescription;
+
+// function showDescription(character) {
+//     marvel.characters
+//       .name(character)
+//       .get(function(err, resp) {
+//         if (err) { console.log("Error: ", err) }
+//         else { console.log(resp[0].description) }
+//       })
+// }
+// module.exports.showDescription = showDescription;
 
 
 // function showDescription(character) {
-//   return marvel.characters
+//    return marvel.characters
 //     .name(character)
 //     .get(function(err, resp) {
 //       if (err) { 
@@ -26,3 +46,4 @@ module.exports.showDescription = showDescription;
 //     });
 // }
 // module.exports.showDescription = showDescription;
+
--- NodejsNpm/node-serieus/tj-modules/app/index.js
+++ NodejsNpm/node-serieus/tj-modules/app/index.js
@@ -1,7 +1,15 @@
+// const character = require('./character')
+
+// const player = "Hulk"
+
+// const description = character.showDescription(player)
+
+// console.log('The description of ' + player + ' is: ' , description);
+
 const character = require('./character')
 
 const player = "Hulk"
 
-const description = character.showDescription(player)
-
-console.log('The description of ' + player + ' is: ' , description);
+character.getDescription(player).
+    then(function (value) { console.log(value) },
+        function (value) { console.log(value) });
