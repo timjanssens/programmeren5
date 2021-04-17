e018fb86eee1ef4718545bf70491cb207af481d3
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-04-17 08:18:09 +0200

    Setup for exercise marvel

---
+++ static-react/components/marvel-page.js
@@ -0,0 +1,44 @@
+class Marvel extends React.Component {
+
+    marvelCharacterStyle = {
+        list: {
+            color: 'green',
+            display: 'flex',
+            flexWrap: 'wrap'
+        },
+        tile: {
+            color: 'white',
+            backgroundColor: 'red',
+            textAlign: 'center',
+            width: '10em',
+            fontSize: '2em',
+            fontFamily: 'Arial',
+            paddingTop: '0.5em'
+        },
+        image: {
+            color: 'blue',
+            width: '10em',
+            paddingTop: '0.5em'
+        }
+    };
+
+    MarvelCharacter(props) {
+        return (<div style={this.marvelCharacterStyle.tile} key={props.name}>
+            {props.name}
+            <img src={props.imageUrl}
+                style={this.marvelCharacterStyle.image} alt={"foto van " + props.name} />
+        </div>);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.props.heading}</h1>
+                <div style={this.marvelCharacterStyle.list}>
+                    {this.props.list.map(element => this.MarvelCharacter(element))}
+                                   
+                </div>
+            </div>
+        );
+    }
+}
